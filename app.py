5db2b54db542081f48b51e15c80576130a560f65
SHRISTI-web <shristis308@gmail.com>
2023-10-10 23:50:00 +0530

    Creating a dictionary of English to Hinglish translations for difficult words

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
+
+translations = {
+    "Definitely": "मझु ेसि",
+    "share": "इस्तमे  ाल करनेके लि े",
+    "feedback": "प्रतिक्रिया",
+    "comment": "टिप्पणी",
+    "So": "तो",
+    "big": "बड़ा",
+    "video": "वीडियो",
+    "clearly": "स्पष  रूपसे",
+    "mention": "उल्लेख करना",
+    "products": "उत्पाद",
+    "waiting": "इंतजार",
+    "bag": "बैग",
+}
